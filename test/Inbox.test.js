2151d39f16161d8473c849de2dc5b970886af2d1
joedoedev <joedoedev@protonmail.com>
2018-09-25 13:56:06 +0100

    Added first beforeEach and describe

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,16 @@ const assert = require('assert');
 const Web3 = require('web3');
 const providerUrl = "http://172.17.0.1:8545";
 web3=new Web3(new Web3.providers.HttpProvider(providerUrl)); // or web3 = new Web3('http://localhost:8545');
+
+beforeEach( ()=>{
+	web3.eth.getCoinbase().then(me=>console.log(me));
+	// web3.eth.getAccounts()
+	// .then(f => {
+	// 	console.log(f);
+	// });
+
+} );
+
+describe('Inbox', () => {
+	it('is capable of deploying contracts\n', ()=>{  })
+});
