79b0ba89e723b375c87ea061bf6d7e82e9106be1
joedoedev <joedoedev@protonmail.com>
2018-09-25 16:25:44 +0100

    Small name fix

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const Web3 = require('web3');
-const providerUrl = "http://172.17.0.1:8545";
-var web3=new Web3(new Web3.providers.HttpProvider(providerUrl)); // or web3 = new Web3('http://localhost:8545');
+const providerUri = "http://172.17.0.1:8545";
+var web3=new Web3(new Web3.providers.HttpProvider(providerUri)); // or web3 = new Web3('http://localhost:8545');
 
 const { interface, bytecode } = require('../compile');
 let abi = JSON.parse(interface); let data = "0x"+bytecode;
