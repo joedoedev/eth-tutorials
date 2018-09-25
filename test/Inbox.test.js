cd2785e3f90a008b7188501de6daa90c2eff3522
joedoedev <joedoedev@protonmail.com>
2018-09-25 14:06:09 +0100

    Clenup

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,17 +1,16 @@
 const assert = require('assert');
 const Web3 = require('web3');
 const providerUrl = "http://172.17.0.1:8545";
-web3=new Web3(new Web3.providers.HttpProvider(providerUrl)); // or web3 = new Web3('http://localhost:8545');
+var web3=new Web3(new Web3.providers.HttpProvider(providerUrl)); // or web3 = new Web3('http://localhost:8545');
 
-beforeEach( ()=>{
-	web3.eth.getCoinbase().then(me=>console.log(me));
-	// web3.eth.getAccounts()
-	// .then(f => {
-	// 	console.log(f);
-	// });
+let me, accounts;
 
+beforeEach( async ()=>{
+	me = await web3.eth.getCoinbase(); //web3.eth.getAccounts().then(f => { console.log(f); });
 } );
 
-describe('Inbox', () => {
-	it('is capable of deploying contracts\n', ()=>{  })
+describe("Inbox\n", () => {
+	it("is capable of deploying contracts", ()=>{ 
+		console.log("eth_coinbase", me, "\n");
+	})
 });
